d63bf0eecb5fd1c23a6e3024ec806ca0e129fa2b
DeaMaduS <deamadus@gmail.com>
2024-03-13 11:35:51 -0300

    Subindo projeto

---
+++ text-generative-ai/Program.cs
@@ -0,0 +1,10 @@
+﻿using OpenAI_API;
+
+var key = new APIAuthentication("YOUR KEY");
+
+var api = new OpenAIAPI(key);
+var prompt = "A long time ago, in a galaxy far, far away...";
+var request = new OpenAI_API.Completions.CompletionRequest(prompt, model: OpenAI_API.Models.Model.Davinci, max_tokens: 100, temperature: 0.7);
+var response = await api.Completions.CreateCompletionAsync(request);
+var completion = response.Completions[0].Text;
+Console.WriteLine("Generated Text: {0}", completion);
